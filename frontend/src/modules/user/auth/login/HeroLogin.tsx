5d99107c583f399da670a5f0e8b405d727bbc37e
Akash28599 <akashkk28599@gmail.com>
2026-04-05 11:50:13 +0530

    pointer

--- frontend/src/modules/user/auth/login/HeroLogin.tsx
+++ frontend/src/modules/user/auth/login/HeroLogin.tsx
@@ -181,7 +181,7 @@ export const HeroLogin = ({ onSwitch }: LoginProps) => {
                     onClick={() => {
                       window.location.href = `${import.meta.env.VITE_API_BASE_URL}/auth/google`;
                     }}
-                    className="flex items-center justify-center gap-2 py-3 px-4 border-2 border-gray-100 rounded-xl hover:bg-gray-50 transition-all font-semibold text-sm text-gray-700"
+                    className="flex items-center justify-center gap-2 py-3 px-4 border-2 border-gray-100 rounded-xl hover:bg-gray-50 transition-all font-semibold text-sm text-gray-700 cursor-pointer"
                   >
                     <svg className="w-5 h-5 font-bold" viewBox="0 0 24 24">
                       <path
@@ -209,7 +209,7 @@ export const HeroLogin = ({ onSwitch }: LoginProps) => {
                     onClick={() => {
                       window.location.href = `${import.meta.env.VITE_API_BASE_URL}/auth/facebook`;
                     }}
-                    className="flex items-center justify-center gap-2 py-3 px-4 border-2 border-[#1877F2] bg-[#1877F2] hover:bg-[#166fe5] rounded-xl transition-all font-semibold text-sm text-white shadow-md shadow-blue-100"
+                    className="flex items-center justify-center gap-2 py-3 px-4 border-2 border-[#1877F2] bg-[#1877F2] hover:bg-[#166fe5] rounded-xl transition-all font-semibold text-sm text-white shadow-md shadow-blue-100 cursor-pointer"
                   >
                     <svg className="w-5 h-5 fill-current" viewBox="0 0 24 24">
                       <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z" />
